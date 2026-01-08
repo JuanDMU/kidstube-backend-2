cb2eef22c9efc0249ded699275ce485d430070ce
Juan Diego Miranda <mirandajuandiego16@gmail.com>
2026-01-08 15:43:26 -0600

    feat(restrictedUser): add RestrictedUser model with schema and PIN hashing

---
+++ src/models/RestrictedUser.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const User = require("./user");
+
+const restrictedUserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        pin: {
+            type: String,
+            required: true,
+            select: false, // This slect hide this value when we fetch user data
+            minlength: 4,
+            maxlength: 6,
+            trim: true,
+        },
+        mainUser: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        
+    },
+    { timestamps: true }
+);
+
+restrictedUserSchema.pre("save", async function(next) {
+  if (this.isModified("pin")) {
+    const salt = await bcrypt.genSalt(10);
+    this.pin = await bcrypt.hash(this.pin, salt);
+  }
+  next();
+});
+
+
+restrictedUserSchema.methods.comparePin = async function (enteredPin) {
+  return await bcrypt.compare(enteredPin, this.pin);
+};
+
+module.exports = mongoose.model("RestrictedUser", restrictedUserSchema);
