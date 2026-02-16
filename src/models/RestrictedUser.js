35cfb613c36f6325c467bc876a719cdc1840ee5a
Juan Diego Miranda <mirandajuandiego16@gmail.com>
2026-02-16 11:53:31 -0600

    Esto es una prueba

--- src/models/RestrictedUser.js
+++ src/models/RestrictedUser.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const User = require("./User");
 
+// Esto es un comentario
+
 const restrictedUserSchema = new mongoose.Schema(
     {
         restrictedUserName: {
